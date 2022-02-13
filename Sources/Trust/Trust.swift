0b8e31f604a4351f91ef1a2a86a7c4bcb493daf5
gitaeklee@doinglab.com <coolfamily77@gmail.com>
2022-02-13 14:50:34 +0900

    trust dci pattern library

--- Sources/Trust/Trust.swift
+++ Sources/Trust/Trust.swift
@@ -1,6 +1,27 @@
-public struct Trust {
-    public private(set) var text = "Hello, World!"
+import Foundation
+#if os(iOS) || os(tvOS)
+  import UIKit
+#endif
 
-    public init() {
+public protocol Trust {}
+
+extension Trust where Self: Any {
+    
+    @inlinable
+    public func describe(_ props: String, _ block: (Self) throws -> Void) rethrows {
+        print("@Describe: \(props) ")
+        try block(self)
+    }
+    
+    @inlinable
+    public func context(_ props: String, _ block: (Self) throws -> Void) rethrows {
+        print("\t @Context: \(props)")
+        try block(self)
+    }
+    
+    @inlinable
+    public func it(_ props: String, _ block: (Self) throws -> Void) rethrows {
+        print("\t\t @It: \(props) \n")
+        try block(self)
     }
 }
--- Tests/TrustTests/TrustTests.swift
+++ Tests/TrustTests/TrustTests.swift
@@ -1,11 +1,33 @@
 import XCTest
 @testable import Trust
 
+struct User {
+    let name: String = "gitaeklee"
+    let email: String = "gitaeklee96@gmail.com"
+}
+
+extension User: Trust { }
+
 final class TrustTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Trust().text, "Hello, World!")
+        // results
+    }
+    
+    func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    func testDCI() {
+        let _ = User().describe("user object", { object in
+            object.context("with 300 and 200", { context in
+                context.it("returns 500", { it in
+                    XCTAssertEqual(it.email, "gitaeklee96@gmail.com")
+                    XCTAssertEqual(it.name, "gitaeklee")
+                    XCTAssertEqual(sum(a: 300, b: 200), 500)
+                })
+            })
+        })
     }
 }
